2d0ef61eff7b0ca60bf7aac092db522a3ef532f7
Bice <59291964+certifiedbice@users.noreply.github.com>
2021-02-20 13:47:28 -0700

    added eslint and prettier

---
+++ src/components/app/App.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<Test></Test>
+	);
+}
+
+export default App;
---
+++ src/components/app/list/list.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import './List.css';
+
+export default class List extends Component{
+	render(){
+		return (
+			'List'
+		);
+	}
+}
