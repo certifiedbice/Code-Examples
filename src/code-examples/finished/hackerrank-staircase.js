5210839db7b39ea97d020a97e758b80d6889fde3
Bice <59291964+certifiedbice@users.noreply.github.com>
2021-02-24 01:15:31 -0700

    Completed hackerrank-staircase.js

--- src/code-examples/finished/hackerrank-staircase.js
+++ src/code-examples/finished/hackerrank-staircase.js
@@ -1,2 +1,31 @@
-let spaces = 0;
-let hashmarks = n;
+function staircase(n) {
+	let spaces = 0;
+	let hashmarks = n;
+	let arr = [];
+
+	while (spaces !== n) {
+		let str = '';
+
+		for (let i = 0; i < spaces; i++) {
+			str += ' ';
+		}
+		
+		for (let i = 0; i < hashmarks; i++) {
+			str += '#';
+		}
+		
+		arr.unshift(str)
+		hashmarks--;
+		spaces++;
+	
+	}
+
+		for (let i = 0; i < arr.length; i++) {
+			console.log(arr[i]);
+		}
+
+
+}
+
+
+staircase(4);
